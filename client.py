b565ae3bac0d39a2d2872abf6b6bb0b5b1a8f8aa
Vladimir NACHEV <893vp893@gmail.com>
2015-02-13 22:50:50 +0200

    fix bugs in communication

--- client.py
+++ client.py
@@ -5,17 +5,24 @@ import socket
 import select
 import getpass
 from hashlib import sha256
-from simplecrypt import encrypt, decrypt
+from simplecrypt import encrypt as encr, decrypt as decr
 
 
 def crypto(x):
-    return str(sha256(x).hexdigest())
+    return sha256(x.encode()).hexdigest()
 
 
 RECV_BUFFER = 4096
 USER = ""
 CYPHER = "PASSWORD"
 
+def encrypt(CYPHER, data):
+    return data.encode()
+    # return encr(CYPHER, data)
+
+def decrypt(CYPHER, data):
+    return data.decode('utf-8')
+    #  return decr(CYPHER, data).decode('utf-8')
 
 def login(log_s):
     global USER
@@ -27,14 +34,13 @@ def login(log_s):
     password = getpass.getpass("<password>:")
     password = crypto(password)
 
-    log_s.send(uname+'^'+password)
+    log_s.send(encrypt(CYPHER, (uname+'^'+password)))
 
     ans = log_s.recv(RECV_BUFFER)
+    ans = decrypt(CYPHER, ans)
     if ans == "SUCCESS":
         USER = uname
         return True
-    elif ans == "FAILED":
-        return False
     else:
         return False
 
@@ -51,7 +57,7 @@ def chat_client():
         host = "127.0.0.1"
     port = 54554
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(10)
+    s.settimeout(100)
 
     try:
         s.connect((host, port))
@@ -66,31 +72,39 @@ def chat_client():
     print('Connected to remote host. You can start sending messages')
     sys.stdout.write('[{}]: '.format(USER))
     sys.stdout.flush()
-     
-    while True:
-        socket_list = [sys.stdin, s]
-        ready_to_read, ready_to_write, in_error = select.select(socket_list, [], [])
-         
-        for sock in ready_to_read:             
-            if sock == s:
-                data = sock.recv(RECV_BUFFER)
-                if not data:
-                    print('\nDisconnected from chat server')
-                    sys.exit()
-                else:
-                    # print data
+
+    socket_list = [sys.stdin, s]
+    try:
+        while True:
+            ready_to_read, ready_to_write, in_error = select.select(socket_list, [], [])
+
+            for sock in ready_to_read:
+                if sock == s:
+                    data = sock.recv(RECV_BUFFER)
                     data = decrypt(CYPHER, data)
-                    sys.stdout.write(data)
+                    if not data:
+                        print('\nDisconnected from chat server')
+                        sys.exit()
+                    else:
+                        # print data
+                        sys.stdout.write(data)
+                        sys.stdout.write('[{}]: '.format(USER))
+                        sys.stdout.flush()
+
+                else:
+                    # user entered a message
+                    msg = sys.stdin.readline()
+                    msg = encrypt(CYPHER, msg)
+                    s.send(msg)
                     sys.stdout.write('[{}]: '.format(USER))
                     sys.stdout.flush()
-            
-            else:
-                # user entered a message
-                msg = sys.stdin.readline()
-                msg = encrypt(CYPHER, msg)
-                s.send(msg)
-                sys.stdout.write('[{}]: '.format(USER))
-                sys.stdout.flush()
+    except KeyboardInterrupt:
+        msg = "LOG OUT"
+        s.send(encrypt(CYPHER, msg))
+        s.close()
+        print("Good bye")
+        sys.exit()
+
 
 if __name__ == "__main__":
     sys.exit(chat_client())
