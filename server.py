74cfbff3dbd89d8448a4876d9db256212b954613
Vladimir NACHEV <893vp893@gmail.com>
2015-02-13 12:17:03 +0200

    adopted from python2 to python3

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import sys
 import socket
 import select
 import pymongo
-from simplecrypt import encrypt,decrypt
+from simplecrypt import encrypt, decrypt
 
 HOST = '0.0.0.0'
 SOCKET_LIST = []
@@ -10,7 +10,8 @@ USERNAMES = dict()
 RECV_BUFFER = 4096
 PORT = 54554
 REG_LOG_PORT = 54555
-CIPHER = "PASSWORD"
+CYPHER = "PASSWORD"
+
 
 def chat_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,7 +28,7 @@ def chat_server():
     SOCKET_LIST.append(server_socket)
     SOCKET_LIST.append(reg_socket)
  
-    print"Server is available on port:" + str(PORT)
+    print("Server is available on port:" + str(PORT))
  
     while True:
 
@@ -42,49 +43,49 @@ def chat_server():
                 buff = sockfd.recv(RECV_BUFFER)
                 if buff != "FAILED":
                     uname, password = buff.split('^')
-                    print "login " + uname
+                    print("login " + uname)
                     users = pymongo.MongoClient().chat.users
-                    cursor = users.find({"username":uname, "password":password})
+                    cursor = users.find({"username": uname, "password": password})
                     if cursor.count() != 1:
-                        print 'FAILED'
+                        print('FAILED')
                         sockfd.send("FAILED")
                         sockfd.close()
                         SOCKET_LIST.remove(sockfd)
 
                     else:
-                        print 'SUCCESS'
+                        print('SUCCESS')
                         USERNAMES[addr] = uname
                         sockfd.send("SUCCESS")
-                        print "Client {}@{}:{} connected".format(USERNAMES[addr],addr[0], addr[1])
+                        print("Client {}@{}:{} connected".format(USERNAMES[addr], addr[0], addr[1]))
                         data = "{}@{}:{} entered our chatting room\n".format(USERNAMES[addr], addr[0], addr[1])
-                        data = encrypt(CIPHER, data)
+                        data = encrypt(CYPHER, data)
                         broadcast(server_socket, reg_socket, sockfd, data)
                 else:
-                    print "Client side failed"
+                    print("Client side failed")
 
-            #register
+            # register
             elif sock == reg_socket:
                 sockfd, addr = reg_socket.accept()
-                print "Client ({}, {}) try to reg ...".format(*addr)
+                print("Client ({}, {}) try to reg ...".format(*addr))
 
                 buff = sockfd.recv(RECV_BUFFER)
                 if buff != "FAILED":
                     uname, password = buff.split('^')
-                    print "register " + " " + uname
+                    print("register " + " " + uname)
                     db_client = pymongo.MongoClient()
                     chat = db_client.chat
                     users = chat.users
 
-                    cursor = users.find({"username":uname})
+                    cursor = users.find({"username": uname})
                     if cursor.count() == 0:
-                        users.insert({"username":uname, "password":password})
-                        print 'SUCCESS'
+                        users.insert({"username": uname, "password": password})
+                        print('SUCCESS')
                         sockfd.send("SUCCESS")
                     else:
-                        print 'FAILED'
+                        print('FAILED')
                         sockfd.send("FAILED")
                 else:
-                    print "Client side FAILED"
+                    print("Client side FAILED")
 
             # message from client
             else:
@@ -92,37 +93,38 @@ def chat_server():
                 try:
                     data = sock.recv(RECV_BUFFER)
                     if data:
-                        data = decrypt(CIPHER,data)
-                        data = "\r" + '[' + USERNAMES[peer]+ '@{}:{}] '.format(*peer) + data
-                        data = encrypt(CIPHER, data)
+                        data = decrypt(CYPHER, data)
+                        data = "\r" + '[' + USERNAMES[peer] + '@{}:{}] '.format(*peer) + data
+                        data = encrypt(CYPHER, data)
                         broadcast(server_socket, reg_socket, sock, data)
                     else:
                         if sock in SOCKET_LIST:
                             SOCKET_LIST.remove(sock)
                             del USERNAMES[peer]
 
-                        data = "Client {}@{}:{} is offline\n".format(USERNAMES[peer],addr[0], addr[1])
-                        data = encrypt(CIPHER, data)
+                        data = "Client {}@{}:{} is offline\n".format(USERNAMES[peer], addr[0], addr[1])
+                        data = encrypt(CYPHER, data)
                         broadcast(server_socket, reg_socket, sock, data)
 
                 # exception 
                 except:
-                    data = "Client {}@{}:{} is offline\n".format(USERNAMES[peer],addr[0], addr[1])
-                    data = encrypt(CIPHER, data)
+                    data = "Client {}@{}:{} is offline\n".format(USERNAMES[peer], addr[0], addr[1])
+                    data = encrypt(CYPHER, data)
                     broadcast(server_socket, reg_socket, sock, data)
                     continue
 
     server_socket.close()
-    
+
+
 # broadcast chat messages to all connected clients
-def broadcast (server_socket, reg_socket, sock, message):
-    print message
+def broadcast(server_socket, reg_socket, sock, message):
+    print(message)
     for socket in SOCKET_LIST:
         # send the message only to peer
-        if socket != server_socket and socket != sock  and socket != reg_socket:
-            try :
+        if socket != server_socket and socket != sock and socket != reg_socket:
+            try:
                 socket.send(message)
-            except :
+            except:
                 # broken socket connection
                 socket.close()
                 # broken socket, remove it
