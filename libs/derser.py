4fc05a283ebde0a51687bf374a8b2e1e18dc016d
Vladimir Nachev <893vp893@gmail.com>
2015-08-25 16:48:29 +0300

    Simplify Message interface
    Remove file transmission
    Add "get_password", "set_username" and "set_type"
    Fix some PEP8 issues

--- libs/derser.py
+++ libs/derser.py
@@ -1,44 +1,58 @@
-'''
+"""
 This is simple serialize/deserialize lib/
 
 It convert builtin.dict to stringed json -> serialize
 and vice versa
 stringed json do dict -> deserialize
-'''
+"""
 from json import dumps, loads
 import time
 
+
 def serialize(dict_arg):
     return dumps(dict_arg, ensure_ascii=False)
+
 
 def deserialize(str_arg):
     return loads(str_arg)
 
 
 class Message:
-    def __init__(self,username, type_of_message):
+    """
+    MESSAGE INTERFACE
+    {
+        "TYPE": ["REGISTER", "LOGIN", "LOGOUT", "MESSAGE", "LIKE",
+                "BLOCK", "UNBLOCK", "20_MORE", "AVATAR"]
+        "DATETIME": ["15:43:55/14.02.2015] -> utc time/gmt+00
+        "STATUS": ["OK", "FAILED"],
+        "USERNAME": "login/register username",
+        "PASSWORD": "password, used only for authentication and registration",
+        "BODY": "TEXT"
+    }
+    """
+    def __init__(self, username, type_of_message):
         self._base_message = dict(TYPE=type_of_message, USERNAME=username)
-        self._base_message["DATETIME"] = time.asctime(time.gmtime())
+        self.set_datetime()
 
     def set_password(self, password):
-        ''' here password is already hashed with sha256'''
-        self._base_message['PASSWORD']=password
+        """ here password is already hashed with sha256"""
+        self._base_message['PASSWORD'] = password
 
     def set_body(self, body):
-        ''' Here body is the text of message or content of transmitted file'''
-        self._base_message['BODY']=body
+        """ Here body is the text of message or content of transmitted file"""
+        self._base_message['BODY'] = body
 
     def set_status(self, status='OK'):
         self._base_message['STATUS'] = status
 
-    def set_chunk_number(self, current_chunk, all_chunks):
-        self._base_message['CHUNK_NUMBER'] = (current_chunk,all_chunks)
-
-    def set_name_of_file(self, filename):
-        self._base_message["FILENAME"] = filename
-
     def set_datetime(self):
         self._base_message["DATETIME"] = time.asctime(time.gmtime())
+
+    def set_username(self, username):
+        self._base_message["USERNAME"] = username
+
+    def set_type(self, message_type):
+        self._base_message["TYPE"] = message_type
 
     def get_type(self):
         return self._base_message['TYPE']
@@ -55,28 +69,11 @@ class Message:
     def get_body(self):
         return self._base_message['BODY']
 
-    def get_chunk_number(self):
-        return  self._base_message['CHUNK_NUMBER']
-
-    def get_filename(self):
-        return self._base_message['FILENAME']
+    def get_password(self):
+        return self._base_message['PASSWORD']
 
     def serialize(self):
         return dumps(self._base_message)
 
     def deserialize(self, recv_message):
         self._base_message = loads(recv_message)
-
-
-'''
-MESSAGE INTERFACE
-{
-    "TYPE": ["REGISTER", "LOGIN", "LOGOUT", "SYNC", "MESSAGE", "LIKE", "BLOCK", "20_MORE", "AVATAR", "RATING", "FILE"],
-    "DATETIME": ["15:43:55/14.02.2015] -> utc time/gmt+00
-    "STATUS": ["OK", "FAILED"],
-    "USERNAME": "login/register username",
-    "PASSWORD": "password, used only for authentication and registration",
-    "BODY": "TEXT"/"content of file"
-    "CHUNK_NUMBER" : current_part/all_parts(11/23 - 11 piece of File devided into 23 parts)
-    "FILENAME" : "hello_world.txt"t
-}'''
